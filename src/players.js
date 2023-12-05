8f2d1d8894354229092eded7451dec42a5f22da6
abdallah taouil <abdallah.taouil@gmail.com>
2023-12-05 17:27:49 +0100

    done

--- src/players.js
+++ src/players.js
@@ -2,8 +2,8 @@ export const players = [
   {
     id: 1,
     name: "modric",
-    team: "real madrid",
-    nationality: "Croatian",
+    team: " Real Madrid",
+    nationality: "nationality:  Croatian",
     jerseyNumber: "number: 10",
     age: "age: 38",
     image:
@@ -12,8 +12,8 @@ export const players = [
   {
     id: 2,
     name: "mbappe",
-    team: "psg",
-    nationality: "frensh",
+    team: "PSG",
+    nationality: "nationality:  frensh",
     jerseyNumber: "number: 7",
     age: "age: 24",
     image: "https://pbs.twimg.com/media/FTSUgXfUEAAa-ZZ.jpg:large",
@@ -21,22 +21,21 @@ export const players = [
   {
     id: 3,
     name: "haland",
-    team: "man city",
-    nationality: "Norwegian",
+    team: "Man City",
+    nationality: "nationality:  Norwegian",
     jerseyNumber: "number: 9",
     age: "age: 23",
     image:
-      "https://cdn.realsport101.com/images/ncavvykf/realsport-production/48d90dad564cf14304b05e49abd8ffdf630268fa-523x827.png?w=523&h=827&auto=format",
+      "https://image-service.onefootball.com/transform?w=280&h=454&dpr=2&image=https%3A%2F%2Fcdn.givemesport.com%2Fwp-content%2Fuploads%2F2022%2F07%2Fimage-89.png",
   },
   {
     id: 4,
     name: "messi",
-    team: "inter miami",
-    nationality: "Argentinian",
+    team: "Inter Miami",
+    nationality: "nationality:  Argentinian",
     jerseyNumber: "number: 30",
     age: "age: 36",
     image:
       "https://preview.redd.it/i-fixed-messis-card-because-he-got-transferred-to-inter-v0-92g6ly32zm4b1.png?width=640&crop=smart&auto=webp&s=02d01427c1faa1b7b6cb87fd8e18b16f59898386",
   },
- 
 ];
